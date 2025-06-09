a8e956f9dfb133f96578b5e18e3835f7da70e55e
rhf007 <rahofahazem512@gmail.com>
2025-06-09 22:32:30 +0300

    set up order controller import

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const { body } = require('express-validator');
 const {protect, admin} = require('../middleware/auth.middleware');
+//TODO: implement order controller
+const {
+    getOrders,
+    getOrder,
+    createOrder,
+    updateOrderStatus
+} = require('../controllers/order.controller');
 const router = express.Router();
 
 //all need a signed in user
