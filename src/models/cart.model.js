38cebf01bc306fc823e7d0e124e59533d877a7d1
AbdelrhmanRabie3 <abdelrhmanrabie33@gmail.com>
2025-06-10 02:08:17 +0300

    Calculate Total Cart Price

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -32,6 +32,23 @@ const cartSchema = new mongoose.Schema(
   }
 );
 
+// Calculate total before saving
+cartSchema.pre('save', async function (next) {
+  try {
+    let total = 0;
+    for (const item of this.items) {
+      const game = await mongoose.model('Game').findById(item.game);
+      if (game) {
+        total += game.price * item.quantity;
+      }
+    }
+    this.total = total;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
 const Cart = mongoose.model('Cart', cartSchema);
 
 module.exports = Cart;
