75c06c17e35d52a8441a3cc20fed7afd76b0df51
AbdelrhmanRabie3 <abdelrhmanrabie33@gmail.com>
2025-06-10 01:51:46 +0300

    Cart model

---
+++ src/models/cart.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    items: [
+      {
+        game: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Game',
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+    total: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Cart = mongoose.model('Cart', cartSchema);
+
+module.exports = Cart;
