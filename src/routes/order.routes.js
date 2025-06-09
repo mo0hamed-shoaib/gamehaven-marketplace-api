8322926b69cebe6cb6e7b7255a0bfd9d5b6b0949
rhf007 <rahofahazem512@gmail.com>
2025-06-09 22:29:48 +0300

    basic happy scenario router

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -2,3 +2,16 @@ const express = require('express');
 const { body } = require('express-validator');
 const {protect, admin} = require('../middleware/auth.middleware');
 const router = express.Router();
+
+//all need a signed in user
+router.use(protect);
+
+//user routes
+router.get('/', getOrders);
+router.get('/:id', getOrder);
+router.post('/', createOrder);
+
+// Admin routes
+router.put('/:id/status', admin, updateOrderStatus);
+
+module.exports = router;
