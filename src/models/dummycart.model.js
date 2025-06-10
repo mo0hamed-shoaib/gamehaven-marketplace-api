3ae6d50844d55d699eca9566a50d6c970fa49974
rhf007 <rahofahazem512@gmail.com>
2025-06-10 03:58:35 +0300

    dummy cart items

--- src/models/dummycart.model.js
+++ src/models/dummycart.model.js
@@ -6,6 +6,29 @@ const dummyCartSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
+    },
+    //cart items
+    //array of cart items
+    //this should be the way these cart items are stored
+    items: [
+        {
+            //reference game id in game model
+            game:{
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Game',
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true,
+                min: 1
+            }
+        }
+    ],
+    total:{
+        type: Number,
+        required: true,
+        default: 0
     }
 });
 const dummyCart = mongoose.model('dummyCart', dummyCartSchema);
