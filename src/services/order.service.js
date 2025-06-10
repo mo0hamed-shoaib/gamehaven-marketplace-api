3fb15d016ddae9a9e47fc60956fad7c13c45d2a5
rhf007 <rahofahazem512@gmail.com>
2025-06-10 04:44:31 +0300

    get single order in service

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -5,7 +5,23 @@ const dummyGame = require('../models/dummyModel.model');
 class OrderService{
     //get user orders
     static async getOrders(userId){
+        //populate method replaces the referenced ObjectId 
+        // in a document with the actual document it refers to
         return Order.find({user: userId}).populate('items.game').sort({ createdAt: -1 });
+    }
+
+    //get single order
+    static async getOrder(userId, orderId){
+        const order = await Order.findOne({
+            _id: orderId,
+            user: userId
+        }).populate('items.game');
+
+        if(!order){
+            throw new Error('Order not found');
+        }
+
+        return order;
     }
 }
 
