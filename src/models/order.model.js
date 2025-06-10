60151e032c76c0b7d276dd3ce8f008d520a1cc4f
rhf007 <rahofahazem512@gmail.com>
2025-06-10 04:22:48 +0300

    order user

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const dummyGame = require('./dummygame.model');
 const dummyCart = require('./dummycart.model')
 const orderSchema = new mongoose.Schema({
-
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
 });
 module.exports = Order;
