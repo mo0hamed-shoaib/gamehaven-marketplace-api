5e111ae63e000999cd047454de907c7444b8397c
rhf007 <rahofahazem512@gmail.com>
2025-06-10 04:38:14 +0300

    get user orders in service

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -2,4 +2,11 @@ const Order = require('../models/order.model');
 const dummyCart = require('../models/dummyCart.model');
 const dummyGame = require('../models/dummyModel.model');
 
+class OrderService{
+    //get user orders
+    static async getOrders(userId){
+        return Order.find({user: userId}).populate('items.game').sort({ createdAt: -1 });
+    }
+}
+
 module.exports = OrderService;
