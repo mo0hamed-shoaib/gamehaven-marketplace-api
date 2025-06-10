a9212434bae779eb8e4c462dd168161afcb6af4c
rhf007 <rahofahazem512@gmail.com>
2025-06-10 03:46:00 +0300

    set up dummy cart model import and export

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -1,6 +1,7 @@
 //TODO: model
 const mongoose = require('mongoose');
 const dummyGame = require('./dummygame.model');
+const dummyCart = require('./dummycart.model')
 const orderSchema = new mongoose.Schema({
 
 });
