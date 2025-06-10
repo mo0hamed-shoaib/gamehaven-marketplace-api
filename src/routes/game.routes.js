b731993b5dcc5b52c262ab51d61d4a537764f33a
rhf007 <rahofahazem512@gmail.com>
2025-06-10 21:44:57 +0300

    hotfix

--- src/routes/game.routes.js
+++ src/routes/game.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { body } = require('express-validator');
 const { protect, admin } = require('../middleware/auth.middleware');
-const { createGame, updateGame, deleteGame } = require('../controllers/dummyGame.controller');
+const { createGame, updateGame, deleteGame } = require('../controllers/game.controller');
 const upload = require('../middleware/upload.middleware');
 
 const router = express.Router();
