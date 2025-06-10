179a37ae7f15dc3a324819b240c0f600344919d7
rhf007 <rahofahazem512@gmail.com>
2025-06-10 03:53:17 +0300

    user part in dummy cart

--- src/models/dummycart.model.js
+++ src/models/dummycart.model.js
@@ -1,2 +1,12 @@
 const mongoose = require('mongoose');
+
+const dummyCartSchema = new mongoose.Schema({
+    //store user id to reference user mdoel
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+});
+const dummyCart = mongoose.model('dummyCart', dummyCartSchema);
 module.exports = dummyCart;
