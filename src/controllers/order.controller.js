1551f2c800ffdd122eb9d9c063015b36cf343dff
rhf007 <rahofahazem512@gmail.com>
2025-06-10 06:18:29 +0300

    get orders in controller

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -2,6 +2,22 @@
 const OrderService = require('../services/order.service');
 const { validationResult } = require('express-validator');
 
+const getOrders = async (req, res) => {
+  try {
+    const orders = await OrderService.getOrders(req.user._id);
+
+    res.json({
+      status: 'success',
+      data: { orders },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   getOrders,
   getOrder,
