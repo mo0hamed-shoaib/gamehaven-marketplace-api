d0385591b37cc8f4d64d400f6640157f8d0ad67e
rhf007 <rahofahazem512@gmail.com>
2025-06-10 04:35:24 +0300

    set up order service imports and export

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -1,1 +1,5 @@
-//TODO: implement order model
+const Order = require('../models/order.model');
+const dummyCart = require('../models/dummyCart.model');
+const dummyGame = require('../models/dummyModel.model');
+
+module.exports = OrderService;
