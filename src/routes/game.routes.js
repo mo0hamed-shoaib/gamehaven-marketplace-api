c1c3af910aa15ef0423201cbfe8bff2bb16ec0e2
rhf007 <rahofahazem512@gmail.com>
2025-06-10 21:29:16 +0300

    basic admin game routes

--- src/routes/game.routes.js
+++ src/routes/game.routes.js
@@ -4,4 +4,11 @@ const {protect, admin} = require('../middleware/auth.middleware');
 const {createGame, updateGame, deleteGame} = require('../controllers/dummyGame.controller');
 const upload = require('../middleware/upload.middleware');
 
+const router = express.Router();
+
+//admin routes
+router.post('/', protect, admin, upload.single('coverImage'), createGame);
+router.put('/:id', protect, admin, upload.single('coverImage'), updateGame);
+router.delete('/:id', protect, admin, deleteGame);
+
 module.exports = router;
