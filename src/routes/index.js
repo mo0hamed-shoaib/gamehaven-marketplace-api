6cf783312646df4febeb545160ef0489ac23092e
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2025-06-17 00:19:19 +0300

    chore: postpone reviews features

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ const authRoutes = require('./auth.routes');
 const userRoutes = require('./user.routes');
 const gameRoutes = require('./game.routes');
 const orderRoutes = require('./order.routes');
-const reviewRoutes = require('./review.routes');
 
 const router = express.Router();
 
@@ -12,6 +11,5 @@ router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/games', gameRoutes);
 router.use('/orders', orderRoutes);
-router.use('/reviews', reviewRoutes);
 
 module.exports = router;
